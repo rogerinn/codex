9829f2209cdfbf63d9473f57b2a3dc5d9ef26549
rogerinn <rogeriosandroju>
2021-12-06 15:29:16 -0300

    fix: change order

--- infra/endpoint-enumeration/http-helper.js
+++ infra/endpoint-enumeration/http-helper.js
@@ -3,28 +3,28 @@ import fetch  from 'node-fetch'
 export const http = { 
 
  POST: async (url, payload, settings = 'application/json') => {
-    fetch(url, { json: { payload }, responseType: settings }) 
-    .then(result => result.status ? result.status !== 404 : result.status)
-    .catch(error => { return })
+    return fetch(url, { json: { payload }, responseType: settings }) 
+      .then(result => result.status ? result.status !== 404 : result.status)
+      .catch(() => { return })
  },
  GET: async url => {
-    fetch(url)
-    .then(result => result.status ? result.status !== 404 : result.status)
-    .catch(error => { return })
+    return fetch(url)
+      .then(result => result.status ? result.status !== 404 : result.status)
+      .catch(() => { return })
  },
  DELETE: async (url, payload, settings = 'application/json') => {
-    fetch(url, { method: 'delete', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-    .then(result => result.status ? result.status !== 404 : result.status)
-    .catch(error => { return })
+    return fetch(url, { method: 'delete', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
+      .then(result => result.status ? result.status !== 404 : result.status)
+      .catch(() => { return })
  },
  PATCH: async (url, payload, settings = 'application/json') => {
-    fetch(url, { method: 'patch', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-    .then(result => result.status ? result.status !== 404 : result.status)
-    .catch(error => { return })
+    return fetch(url, { method: 'patch', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
+      .then(result => result.status ? result.status !== 404 : result.status)
+      .catch(() => { return })
  },
  PUT: async (url, payload, settings = 'application/json') => {
-   fetch(url, { method: 'put', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-   .then(result => result.status ? result.status !== 404 : result.status)
-   .catch(error => { return })
+    return fetch(url, { method: 'put', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
+      .then(result => result.status ? result.status !== 404 : result.status)
+      .catch(() => { return })
  }
 }
