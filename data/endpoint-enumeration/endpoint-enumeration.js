0811a10c42f487a4333d64062e9a0632a2fe6f9b
rogerinn <rogeriosandroju>
2021-12-06 23:32:17 -0300

    feat: ensure mainProcess returns percent of process

--- data/endpoint-enumeration/endpoint-enumeration.js
+++ data/endpoint-enumeration/endpoint-enumeration.js
@@ -1,9 +1,10 @@
 export const mainProcess = (wordlist, http) => { 
   let result = []
+  let percent = 0
   const main = async input => {
     console.info('Buscando....')
     for(const word of wordlist){
-      if(word) console.info(`Buscando com: ${word}`)
+      console.log(`Total concluído: ${(percent = percent += 100/wordlist.length).toFixed(3)}%...`)
       for(const method of [http.GET, http.POST, http.DELETE, http.PATCH, http.PUT]){
         const isValid = await method(`${input}/${word}`)
         if(isValid) result.push({ url: `${input}/${word}`, statusCode: isValid, method })
