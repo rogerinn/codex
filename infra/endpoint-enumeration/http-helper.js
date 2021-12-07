fdf2dd9bd41e4cb1d30d853e43b7c63811aadff3
rogerinn <rogeriosandroju>
2021-12-06 23:31:40 -0300

    refactor: ensure http helper return status code

--- infra/endpoint-enumeration/http-helper.js
+++ infra/endpoint-enumeration/http-helper.js
@@ -4,27 +4,27 @@ export const http = {
 
  POST: async (url, payload, settings = 'application/json') => {
     return fetch(url, { json: { payload }, responseType: settings }) 
-      .then(result => result.status ? result.status !== 404 : result.status)
+      .then(result => { if(result.status != 404) return result.status })
       .catch(() => { return })
  },
  GET: async url => {
     return fetch(url)
-      .then(result => result.status ? result.status !== 404 : result.status)
+      .then(result => { if(result.status != 404) return result.status })
       .catch(() => { return })
  },
  DELETE: async (url, payload, settings = 'application/json') => {
     return fetch(url, { method: 'delete', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-      .then(result => result.status ? result.status !== 404 : result.status)
+      .then(result => { if(result.status != 404) return result.status })
       .catch(() => { return })
  },
  PATCH: async (url, payload, settings = 'application/json') => {
     return fetch(url, { method: 'patch', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-      .then(result => result.status ? result.status !== 404 : result.status)
+      .then(result => { if(result.status != 404) return result.status })
       .catch(() => { return })
  },
  PUT: async (url, payload, settings = 'application/json') => {
     return fetch(url, { method: 'put', body: JSON.stringify(payload), headers: {'Content-Type': settings } })
-      .then(result => result.status ? result.status !== 404 : result.status)
+      .then(result => { if(result.status != 404) return result.status })
       .catch(() => { return })
  }
 }
